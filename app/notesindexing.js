6fb8a96bc9fce040071eda5af1dd1462042e9b52
Max Starikov <mstarikov@itemis.com>
2023-12-14 14:29:13 +0100

    add config for the project, closes #3

--- app/notesindexing.js
+++ app/notesindexing.js
@@ -1,19 +1,14 @@
 import NotesIndexer from "./src/notesindexer.js";
+import fs from 'fs';
+
+let config = JSON.parse(fs.readFileSync("./notes.config.json", 'utf-8'))
 
 const indexer = new NotesIndexer('../notes')
 
-indexer.setTagSynonymes({
-    programming: ["программирование"],
-    "продуктивность": ["productivity"],
-    checklist: ["checklist", "чеклисты", "checklists", "чеклист"],
-    lists: ["список", "list", "lists", "списки"],
-    books: ["books", "книги"],
-    projects: ["projects", "проекты"],
-    japanese: ["японский"],
-    persons: ["personen", "люди", "персоны"],
-    languages: ["languages", "languages", "языки", "язык", "language", "sprache", "sprachen"]
-})
+indexer.setTagSynonymes(config)
 
 indexer.indexPages()
+
+
 
 
